8a124d66d6edbce2bf0985ac95e4b644a9b2080e
mcogiel <mcogiel@172.16.100.44>
2015-05-21 09:12:42 +0200

    Zabezpieczenia w klasie Pracownik

--- src/zaliczenie/Zaliczenie.java
+++ src/zaliczenie/Zaliczenie.java
@@ -1,9 +1,60 @@
 package zaliczenie;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Zaliczenie {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Lista p = new Lista();
+        p.dodajPracownika("Michał", "Cogiel", 'M', 5, 3000, 25, 0, false);
+        p.dodajPracownika("Marcin", "Cogiel", 'M', 5, 2000, 26, 0, false);
+        int wybor;
+        Menu men = new Menu();
+        wybor = men.menu();
+        while (wybor != 0) {
+            switch (wybor) {
+                case 1:
+                    p.wyswietlPracownikow();
+                    break;
+                case 2:                    
+                    p.dodajPracownika(men.dodawniePrac());
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+
+                default:
+                    System.out.println("Wybrano złą opcje");
+
+            }
+
+            System.out.println("\nWciśnij Enter, aby kontynuować...");
+            System.in.read();
+
+            wybor = Menu.menu();
+        }
+        /*        Pracownik p1 = new Pracownik("Michał", "Cogiel", 'M', 5, 3000, 25, 0, false);
+         p1.podwyzka(5);
+         Lista p = new Lista();
+         p.dodajPracownika("Michał", "Cogiel", 'M', 5, 3000, 25, 0, false);
+         p.dodajPracownika("Marcin", "Cogiel", 'M', 5, 2000, 26, 0, false);
+         p.wyswietlanie();
+         p.wyswietlanieOkrojone();
+         p.wyswietlanieSpecjalne();*/
     }
-    
+
 }
